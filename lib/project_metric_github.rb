eae51fc5077c517c1d277031e30990d47525497f
Sam Joseph <tansaku@gmail.com>
2016-07-28 18:42:28 +0900

    adds raw_data and refresh methods

--- lib/project_metric_github.rb
+++ lib/project_metric_github.rb
@@ -6,11 +6,32 @@ class ProjectMetricGithub
     @client = Octokit::Client.new(access_token: ENV['GITHUB_KEY'])
   end
 
+  # def initialize credentials = {}, raw_data = nil
+  #   @identifier = "github#{URI::parse(credentials[:url]).path}"
+  #   @raw_data = raw_data
+  # end
+
   def image
     # some visual representation of github activity
   end
 
   def score
-    @client.search_issues("repo:#{@identifier} type:pr")['total_count']
+    @raw_data ||= get_pull_requests
+    @raw_data['total_count']
+  end
+
+  def raw_data=(raw_data)
+    @raw_data = raw_data
+  end
+
+  def refresh
+    @raw_data = get_pull_requests
+    true
+  end
+
+  private
+
+  def get_pull_requests
+    @client.search_issues("repo:#{@identifier} type:pr")
   end
 end
--- spec/github_project_metrics_spec.rb
+++ spec/github_project_metrics_spec.rb
@@ -1,22 +1,38 @@
 require 'project_metric_github'
 
 describe ProjectMetricGithub, :vcr  do 
+
+  let(:raw_data) { JSON.parse File.read './spec/data/github_search_issues_ls_repo_type_pr_314.json' }
+
   context 'AgileVentures/WebsiteOne repo' do
     subject(:github_project_metrics) do
       described_class.new url: 'http://github.com/AgileVentures/WebsiteOne'
     end
 
-    it 'has the corresponding scalar value' do
+    it 'has the corresponding score value' do
       expect(github_project_metrics.score).to eq 940
     end
   end
+
   context 'AgileVentures/LocalSupport repo' do
     subject(:github_project_metrics) do
       described_class.new url: 'http://github.com/AgileVentures/LocalSupport'
     end
 
-    it 'has the corresponding scalar value' do
+    it 'has the corresponding score value' do
       expect(github_project_metrics.score).to eq 315
     end
+
+    it 'can have raw data set directly, avoiding network request', vcr: false do
+      github_project_metrics.raw_data = raw_data
+      expect(github_project_metrics.score).to eq 314
+    end
+
+    it 'can have network data refreshed, forcing network request' do
+      github_project_metrics.raw_data = raw_data
+      github_project_metrics.refresh
+      expect(github_project_metrics.score).to eq 315
+    end
+
   end
 end
