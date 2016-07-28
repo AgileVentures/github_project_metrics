1ad733d0563755ba6425b1d0518b7da5a8683db2
Sam Joseph <tansaku@gmail.com>
2016-07-28 18:43:41 +0900

    oops, forgot a load of files and need to bump gem version

--- lib/project_metric_github.rb
+++ lib/project_metric_github.rb
@@ -6,11 +6,6 @@ class ProjectMetricGithub
     @client = Octokit::Client.new(access_token: ENV['GITHUB_KEY'])
   end
 
-  # def initialize credentials = {}, raw_data = nil
-  #   @identifier = "github#{URI::parse(credentials[:url]).path}"
-  #   @raw_data = raw_data
-  # end
-
   def image
     # some visual representation of github activity
   end
