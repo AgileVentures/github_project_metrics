4ce264b4332e191010e5dcb7134e194a1d213383
Sam Joseph <tansaku@gmail.com>
2016-08-02 22:52:00 +0900

    completes first pass of getting full original projectscope github metric

--- spec/github_project_metrics_spec.rb
+++ spec/github_project_metrics_spec.rb
@@ -3,9 +3,9 @@ require 'project_metric_github'
 describe ProjectMetricGithub, :vcr  do 
 
   let(:raw_data) { JSON.parse(File.read('./spec/data/github_search_issues_ls_repo_type_pr_314.json'), object_class: OpenStruct) }
-  let(:svg_wso_two_point_five) { File.read './spec/data/websiteone_940_pulls.svg' }
-  let(:svg_ls_six_point_one_six) { File.read './spec/data/localsupport_315_pulls.svg' }
-  let(:svg_output_314) { File.read './spec/data/localsupport_314_pulls.svg' }
+  let(:svg_wso_66_11) { File.read './spec/data/websiteone_66_11_pulls.svg' }
+  let(:svg_ls_27_18) { File.read './spec/data/localsupport_27_18_pulls.svg' }
+  let(:svg_ls_30_46) { File.read './spec/data/localsupport_30_46_pulls.svg' }
 
   context 'AgileVentures/WebsiteOne repo' do
     subject(:github_project_metrics) do
@@ -17,7 +17,7 @@ describe ProjectMetricGithub, :vcr  do
     end
 
     it 'has the corresponding image value' do
-      expect(github_project_metrics.image).to eq svg_wso_two_point_five
+      expect(github_project_metrics.image).to eq svg_wso_66_11
     end
   end
 
@@ -31,20 +31,20 @@ describe ProjectMetricGithub, :vcr  do
     end
 
     it 'has the corresponding image value' do
-      expect(github_project_metrics.image).to eq svg_ls_six_point_one_six
+      expect(github_project_metrics.image).to eq svg_ls_27_18
     end
 
     it 'can have raw data set directly, avoiding network request', vcr: false do
       github_project_metrics.raw_data = raw_data
       expect(github_project_metrics.score).to eq 0.6166666666666667
-      expect(github_project_metrics.image).to eq svg_output_314
+      expect(github_project_metrics.image).to eq svg_ls_30_46
     end
 
     it 'can have network data refreshed, forcing network request' do
       github_project_metrics.raw_data = raw_data
       github_project_metrics.refresh
       expect(github_project_metrics.score).to eq 0.6328125
-      expect(github_project_metrics.image).to eq svg_ls_six_point_one_six
+      expect(github_project_metrics.image).to eq svg_ls_27_18
     end
 
   end
